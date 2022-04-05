a07b97931759d5fe67db61ca98e97671f54f663f
Bethsyf <bethsyfalcon@gmail.com>
2022-04-05 07:20:02 -0500

    html usuarios

--- scripts/principal.js
+++ scripts/principal.js
@@ -1,9 +1,14 @@
 import {getData} from "./getData.js"
 import { showData } from "./showData.js"
 
-let btnMarvel = document.getElementById("btnCategory1")
-let btnDC = document.getElementById("btnCategory2")
-let listarCard = document.getElementById("listarCard")
+let btnMarvel = document.getElementById("btnCategory1");
+let btnDC = document.getElementById("btnCategory2");
+let listarCard = document.getElementById("listarCard");
+let btnGuardar = document.getElementById('btnGuardar');
+let btnCorreo = document.getElementById('btnCorreo');
+let btnEditar = document.getElementById('btnEditar');
+let btnEliminar = document.getElementById('btnEliminar');
+let formulario = document.getElementById('formulario');
 
 
 
