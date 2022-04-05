30ed40c47c5c0c2f4bb58ba2a7fc80c8315dee14
Bethsyf <bethsyfalcon@gmail.com>
2022-04-05 05:50:16 -0500

    ejercicio practica

---
+++ scripts/principal.js
@@ -0,0 +1,18 @@
+import {getData} from "./getData.js"
+import { showData } from "./showData.js"
+
+let btnMarvel = document.getElementById("btnCategory1")
+let btnDC = document.getElementById("btnCategory2")
+let listarCard = document.getElementById("listarCard")
+
+
+
+btnMarvel.addEventListener('click', async()=>{
+    const array = await getData ('https://api-ejercicio-axios.herokuapp.com/marvel')
+    showData(array, listarCard)
+})
+
+btnDC.addEventListener('click', async()=>{
+    const array =  await getData('https://api-ejercicio-axios.herokuapp.com/dc')
+    showData(array, listarCard)
+})
